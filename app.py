948c38ad276356e3a4481939d6ede9c0dbbab304
teddiur <ryaqueiroz@gmail.com>
2021-04-25 22:02:35 -0300

    adding routes with no implementation

--- app.py
+++ app.py
@@ -2,10 +2,52 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        'name': "my first store",
+        'items': [
+            {
+                'name': 'simple item',
+                'price': 10.5
+            }
+        ]
+    }
+]
+# POST /store {name:}
 
-@app.route('/')
-def home():
-    return "Hello, world"
+
+@app.route('/store', methods=["POST"])
+def create_store():
+    pass
+
+# GET /store/<string:name>
+
+
+@app.route('/store/<string:name>', methods=["GET"])
+def get_store(name):
+    pass
+
+# GET /store
+
+
+@app.route('/store', methods=["GET"])
+def get_all_stores():
+    pass
+
+
+# POST /store/<string:name>/item {name: , price:}
+
+
+@app.route('/store/<string:name>/item', methods=["POST"])
+def create_item(name):
+    pass
+
+# GET /store/<string:name>/item
+
+
+@app.route('/store', methods=["GET"])
+def get_items_in_store(name):
+    pass
 
 
 app.run(port=19000)
