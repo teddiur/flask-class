0a529c1bd882c56884a0ac48f4e6b453a506e4f1
teddiur <ryaqueiroz@gmail.com>
2021-04-29 21:26:36 -0300

    implemented other endpoints

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 stores = [
     {
-        'name': "my first store",
-        'items': [
+        "name": "my first store",
+        "items": [
             {
-                'name': 'simple item',
-                'price': 10.5
+                "name": "simple item",
+                "price": 10.5
             }
         ]
     }
@@ -16,38 +16,66 @@ stores = [
 # POST /store {name:}
 
 
-@app.route('/store', methods=["POST"])
+@app.route("/store", methods=["POST"])
 def create_store():
-    pass
+    request_data = request.get_json()
+    name = request_data.get("name")
+    new_store = {
+        "name": request_data["name"],
+        "items": []}
+    if any(name == store["name"] for store in stores):
+        return jsonify(
+            {"error": 409, "message": "this store already exists"})
+    else:
+        stores.append(new_store)
+        return jsonify({"status": "success", "store": new_store})
 
 # GET /store/<string:name>
 
 
-@app.route('/store/<string:name>', methods=["GET"])
+@app.route("/store/<string:name>", methods=["GET"])
 def get_store(name):
-    pass
+    for store in stores:
+        if store["name"] == name:
+            return jsonify({"status": "success", "store": store})
+    return jsonify({"error": 404, "message": "Store not found"})
 
 # GET /store
 
 
-@app.route('/store', methods=["GET"])
+@app.route("/store", methods=["GET"])
 def get_all_stores():
-    pass
+    return jsonify({"stores": stores})
 
 
+def get_store(store_name):
+    for store in stores:
+        if store["name"] == store_name:
+            return store
 # POST /store/<string:name>/item {name: , price:}
 
 
-@app.route('/store/<string:name>/item', methods=["POST"])
+@app.route("/store/<string:name>/item", methods=["POST"])
 def create_item(name):
-    pass
+    request_data = request.get_json()
+    store_to_add = get_store(name)
+    try:
+        item = {"name": request_data["name"],
+                "price": request_data["price"]}
+        store_to_add["items"].append(item)
+        return jsonify({"item": item, "status": "success"})
+    except NameError:
+        return jsonify({"error": 404, "message": "store doesn't exist"})
 
 # GET /store/<string:name>/item
 
 
-@app.route('/store', methods=["GET"])
+@app.route("/store/<string:name>/item", methods=["GET"])
 def get_items_in_store(name):
-    pass
+    searched_store = get_store(name)
+    if (searched_store):
+        return jsonify({"items": searched_store["items"]})
+    return jsonify({"error": 404, 'message': 'store not found'})
 
 
 app.run(port=19000)
